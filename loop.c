bb7db24d7c9e13dcaa15508726beac80ec90eb22
avikshark777 <avikshark777@gmail.com>
2024-09-07 16:44:41 +0000

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for ( i=1 ; i<=10 ; i++ )
+
+{
+
+   printf("10 bar koilei ki hybo ek kotha??\n");
+
+   printf("tao kokmu,loop shikhte hbe.\n");
+
+   printf("ok\n");
+
+   printf("%d\n",i);
+}
+
+    return 0;
+
+}
